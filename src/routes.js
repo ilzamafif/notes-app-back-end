c4bad133c3234009bb47361820c1f4062d27f7c5
ilzamafif <ilzamafif27@gmail.com>
2021-05-18 19:12:55 +0700

    mengubah catatan

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,24 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require('./handler');
 const routes = [
   {
     method: 'POST',
     path: '/notes',
-    routes: {
-    cors: {
-      origin: ['*'],
-    },
+    handler: addNoteHandler,
   },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+   {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
   },
 ];
 
